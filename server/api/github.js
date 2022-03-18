293cf78cd2ca902499640e958c23c549eaa219cd
Fernando Ortega <feortegas@gmail.com>
2022-03-17 22:31:36 -0500

    removed api function calls to prevent running out of api rate limit

--- server/api/github.js
+++ server/api/github.js
@@ -1,41 +1,44 @@
 const fetch = require('node-fetch');
-// import { fetch } from 'node-fetch';
-// const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const userName = 'feortegas';
 const repoName = 'OnTrack';
 // const projName = 'OnTrack';
 
 // fetch user repos from the api url
 const getRepos = (user) => {
-  // format the github api url
-  const apiUrl = `https://api.github.com/users/${user}/repos`;
+	// format the github api url
+	const apiUrl = `https://api.github.com/users/${user}/repos`;
 
-  // make a request to the url
-  fetch(apiUrl)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(
-        'repo name is: ' + data[0].name + ' and the url is: ' + data[0].html_url
-      );
-    });
+	// make a request to the url
+	fetch(apiUrl)
+		.then((res) => res.json())
+		.then((data) => {
+			for (let index = 0; index < data.length; index++) {
+				console.log(
+					'repo name is: ' +
+						data[index].name +
+						' and the url is: ' +
+						data[index].html_url
+				);
+			}
+		});
 };
 
 // fetch user repos from the api url
 const getUser = (user) => {
-  // format the github api url
-  const apiUrl = `https://api.github.com/users/${user}`;
+	// format the github api url
+	const apiUrl = `https://api.github.com/users/${user}`;
 
-  // make a request to the url
-  fetch(apiUrl)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(
-        'user login is: ' +
-          data.login +
-          ' and the avatar url is: ' +
-          data.avatar_url
-      );
-    });
+	// make a request to the url
+	fetch(apiUrl)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(
+				'user login is: ' +
+					data.login +
+					' and the avatar url is: ' +
+					data.avatar_url
+			);
+		});
 };
 
 // fetch repo projects from the api url - NOT ACTIVE ONCE REQUIRES AUTHENTICATION
@@ -52,28 +55,30 @@ const getUser = (user) => {
 
 // fetch repository issues from the api url
 const getIssues = (user, repo) => {
-  const apiUrl = `https://api.github.com/repos/${user}/${repo}/issues`;
-  // make a request to the url
-  fetch(apiUrl)
-    .then((res) => res.json())
-    .then((data) => {
-      for (let index = 0; index < data.length; index++) {
-        console.log(
-          'repo name is: ' +
-            data[index].title +
-            ' and the assigned dev is: ' +
-            data[index].assignee.login
-        );
-      }
-    });
+	const apiUrl = `https://api.github.com/repos/${user}/${repo}/issues`;
+	// make a request to the url
+	fetch(apiUrl)
+		.then((res) => res.json())
+		.then((data) => {
+			for (let index = 0; index < data.length; index++) {
+				if (data[index].assignee != null) {
+					console.log(
+						'repo name is: ' +
+							data[index].title +
+							' and the assigned dev is: ' +
+							data[index].assignee.login
+					);
+				}
+			}
+		});
 };
 
-getRepos(userName);
+// getRepos(userName);
 
 // getProjects(repoName);
 
-getIssues(userName, repoName);
+// getIssues(userName, repoName);
 
-getUser(userName);
+// getUser(userName);
 
 module.exports = { getRepos, getUser, getIssues };

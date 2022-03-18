9e07f459033cca534a3d18089cfca53a91dd5a68
Jojo Bautista <gjojob@yahoo.com>
2022-03-17 19:09:08 -0500

    added Schema's typeDevs and resolvers. Also tested Add User and Login in the server side

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 // });
 
 db.once('open', () => {
-	app.listen(PORT, () => {
-		console.log(`API server running on port ${PORT}!`);
-	});
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });

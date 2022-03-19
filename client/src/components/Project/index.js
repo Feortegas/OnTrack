55fe73dfee42ae5e4b47e0fce2b90987a75f14dc
Fernando Ortega <feortegas@gmail.com>
2022-03-18 21:00:12 -0500

    finished adding backend data to Project component

--- client/src/components/Project/index.js
+++ client/src/components/Project/index.js
@@ -23,25 +23,34 @@ function Project() {
 
 	return (
 		<>
-			<section className='section  '>
-				<h1 className='has-text-centered has-text-weight-semibold is-size-3 project-title'>
-					Current Project
-				</h1>
-				<div className='center section'>
-					<h2 className='font'>{projects[0].projectTitle}</h2>
-				</div>
-				<div className='section center'>
-					<h3 className='font'>Select a new project</h3>
-				</div>
-				<div className='center '>
-					<button
-						className='button is-info is-rounded'
-						onClick={() => toggleModal()}>
-						<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-					</button>
-				</div>
-				<div>{isModalOpen && <Modal onClose={toggleModal} />}</div>
-			</section>
+			<div>
+				{loading ? (
+					<div>Loading...</div>
+				) : (
+					<section className='section'>
+						<h1 className='has-text-centered has-text-weight-semibold is-size-3 project-title'>
+							Current Project
+						</h1>
+						<div className='center section'>
+							{/* hardcoding index 0 once we are only handling 1 project at a time on our MVP */}
+							<h2 className='font'>{projects[0].projectTitle}</h2>
+							{/* temporary field to display data of issueCount for testing */}
+							<p>Issue count is: {projects[0].issueCount}</p>
+						</div>
+						<div className='section center'>
+							<h3 className='font'>Select a new project</h3>
+						</div>
+						<div className='center'>
+							<button
+								className='button is-info is-rounded'
+								onClick={() => toggleModal()}>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</button>
+						</div>
+						<div>{isModalOpen && <Modal onClose={toggleModal} />}</div>
+					</section>
+				)}
+			</div>
 		</>
 	);
 }

400c3e2cf42fdd281d0cb72fef47378411041140
Edison Simondet <edison.simondet@gmail.com>
2022-03-18 19:10:43 -0500

    dashboard skeleton

---
+++ client/src/components/Issues/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Issues() {
+  return (
+    <>
+      <section>
+        {/* remove placeholder once real development on component begins */}
+
+        <div className='section '>
+          <h1 className='title'>Issues</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Issues;

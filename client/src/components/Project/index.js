313ed9990c1b41c5e557ec55e39ba0e7d1751c46
Fernando Ortega <feortegas@gmail.com>
2022-03-18 20:32:13 -0500

    add Project data from DB to Project component

--- client/src/components/Project/index.js
+++ client/src/components/Project/index.js
@@ -3,39 +3,47 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import './project.css';
 import Modal from '../ProjectModal';
+import { useQuery } from '@apollo/client';
+import { QUERY_PROJECTS } from '../../utils/queries';
 
 function Project() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const toggleModal = () => {
-    setIsModalOpen(!isModalOpen);
-  };
+	const toggleModal = () => {
+		setIsModalOpen(!isModalOpen);
+	};
 
-  return (
-    <>
-      <section className='section  '>
-        <h1 className='has-text-centered has-text-weight-semibold is-size-3 project-title'>
-          Current Project
-        </h1>
-        <div className='center section'>
-          {/* future task, change this to display current project for user if project is selected */}
-          <h2 className='font'>No project selected</h2>
-        </div>
-        <div className='section center'>
-          <h3 className='font'>Select a new project</h3>
-        </div>
-        <div className='center '>
-          <button
-            className='button is-info is-rounded'
-            onClick={() => toggleModal()}
-          >
-            <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-          </button>
-        </div>
-        <div>{isModalOpen && <Modal onClose={toggleModal} />}</div>
-      </section>
-    </>
-  );
+	// use useQuery hook to make query request
+	const { loading, data } = useQuery(QUERY_PROJECTS);
+	const projects = data?.projects || [];
+
+	if (!projects.length) {
+		return <h3>No projects yet</h3>;
+	}
+
+	return (
+		<>
+			<section className='section  '>
+				<h1 className='has-text-centered has-text-weight-semibold is-size-3 project-title'>
+					Current Project
+				</h1>
+				<div className='center section'>
+					<h2 className='font'>{projects[0].projectTitle}</h2>
+				</div>
+				<div className='section center'>
+					<h3 className='font'>Select a new project</h3>
+				</div>
+				<div className='center '>
+					<button
+						className='button is-info is-rounded'
+						onClick={() => toggleModal()}>
+						<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+					</button>
+				</div>
+				<div>{isModalOpen && <Modal onClose={toggleModal} />}</div>
+			</section>
+		</>
+	);
 }
 
 export default Project;
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,25 +1,45 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
+	mutation login($email: String!, $password: String!) {
+		login(email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
+	mutation addUser($username: String!, $email: String!, $password: String!) {
+		addUser(username: $username, email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
+`;
+
+export const ADD_PROJECT = gql`
+	mutation addProject(
+		$projectID: String!
+		$projectTitle: String!
+		$projectUrl: String!
+	) {
+		addProject(
+			projectID: $projectID
+			projectTitle: $projectTitle
+			projectURL: $projectUrl
+		) {
+			_id
+			projectID
+			projectTitle
+			projectURL
+			completionDate
+		}
+	}
 `;

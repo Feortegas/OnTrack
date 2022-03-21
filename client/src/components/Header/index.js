c08c428a93b7a6714bbf459de358303ab8d53922
Edison Simondet <edison.simondet@gmail.com>
2022-03-20 22:57:58 -0500

    fixing sign up page

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChartGantt } from '@fortawesome/free-solid-svg-icons';
 import './header.css';
 import Login from '../Login';
+import Auth from '../../utils/auth';
 
 function Header() {
   const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
@@ -13,69 +14,73 @@ function Header() {
   };
 
   return (
-    <header className="background-color-secondary">
+    <header className='background-color-secondary'>
       <div>
-        <nav className="navbar outline primary">
+        <nav className='navbar outline primary'>
           {/* Hamburger menu */}
           <div
-            role="button"
+            role='button'
             className={`navbar-burger font${
               isHamburgerOpen ? 'is-active' : ''
             }`}
-            aria-label="menu"
-            aria-expanded="false"
+            aria-label='menu'
+            aria-expanded='false'
             onClick={() => toggleNav()}
           >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
+            <span aria-hidden='true'></span>
+            <span aria-hidden='true'></span>
+            <span aria-hidden='true'></span>
           </div>
 
           {/* Menu content */}
-          <div
-            id="navMenu"
-            className={`navbar-menu primary ${
-              isHamburgerOpen ? 'is-active' : ''
-            }`}
-          >
-            <div className="navbar-start primary">
-              <Link
-                to="/dashboard"
-                className="navbar-item font primary"
-                onClick={() => toggleNav()}
-              >
-                Dashboard
-              </Link>
-              <Link
-                to="/project"
-                className="navbar-item font primary"
-                onClick={() => toggleNav()}
-              >
-                Projects
-              </Link>
-              <Link
-                to="/team"
-                className="navbar-item font primary"
-                onClick={() => toggleNav()}
-              >
-                Team
-              </Link>
-              <Link
-                to="/issues"
-                className="navbar-item font primary"
-                onClick={() => toggleNav()}
-              >
-                Issues
-              </Link>
+          {Auth.loggedIn() ? (
+            <div
+              id='navMenu'
+              className={`navbar-menu primary ${
+                isHamburgerOpen ? 'is-active' : ''
+              }`}
+            >
+              <div className='navbar-start primary'>
+                <Link
+                  to='/dashboard'
+                  className='navbar-item font primary'
+                  onClick={() => toggleNav()}
+                >
+                  Dashboard
+                </Link>
+                <Link
+                  to='/project'
+                  className='navbar-item font primary'
+                  onClick={() => toggleNav()}
+                >
+                  Projects
+                </Link>
+                <Link
+                  to='/team'
+                  className='navbar-item font primary'
+                  onClick={() => toggleNav()}
+                >
+                  Team
+                </Link>
+                <Link
+                  to='/issues'
+                  className='navbar-item font primary'
+                  onClick={() => toggleNav()}
+                >
+                  Issues
+                </Link>
+              </div>
             </div>
-          </div>
+          ) : (
+            ''
+          )}
         </nav>
 
         {/* Website Title */}
-        <div className="element header-container">
-          <div className="burger-placeholder"></div>
-          <h1 className="title">
-            <Link to="/dashboard" className="white-text">
+        <div className='element header-container'>
+          <div className='burger-placeholder'></div>
+          <h1 className='title'>
+            <Link to='/dashboard' className='white-text'>
               <FontAwesomeIcon icon={faChartGantt}></FontAwesomeIcon>
               &nbsp;OnTrack
             </Link>
--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import './login.css';
@@ -27,7 +26,7 @@ function Login() {
   };
 
   // submit form
-  const handleFormSubmit = async (event) => {
+  const handleLogin = async (event) => {
     toggleSign();
     try {
       const { data } = await login({
@@ -46,76 +45,84 @@ function Login() {
     });
   };
 
+  const logOut = async (event) => {
+    Auth.logout();
+  };
+
   return (
     <>
-      <div className={`dropdown is-right ${isSignOpen ? 'is-active' : ''}`}>
-        <div className="dropdown-trigger">
-          <button
-            className="button navSign primary font"
-            aria-haspopup="true"
-            aria-controls="dropdown-menu6"
-            onClick={() => toggleSign()}
-          >
-            <span>Sign In</span>
-            <span className="icon is-small">
-              <i aria-hidden="true">
-                <FontAwesomeIcon icon={faAngleDown}></FontAwesomeIcon>
-              </i>
-            </span>
-          </button>
-        </div>
-        <div className="dropdown-menu" id="dropdown-menu6" role="menu">
-          <div className="dropdown-content accent">
-            <div className="dropdown-item">
-              <form onSubmit={handleFormSubmit}>
-                <div className="field">
-                  <label className="label font">Email</label>
-                  <div className="control">
-                    <input
-                      className="input primary font"
-                      placeholder="Your email"
-                      name="email"
-                      type="email"
-                      id="login_email"
-                      value={formState.email}
-                      onChange={handleChange}
-                    />
+      {Auth.loggedIn() ? (
+        <button
+          className='button navSign primary font'
+          aria-haspopup='true'
+          aria-controls='dropdown-menu6'
+          onClick={() => logOut()}
+        >
+          Sign Out
+        </button>
+      ) : (
+        <div className={`dropdown is-right ${isSignOpen ? 'is-active' : ''}`}>
+          <div className='dropdown-trigger'>
+            <button
+              className='button navSign primary font'
+              aria-haspopup='true'
+              aria-controls='dropdown-menu6'
+              onClick={() => toggleSign()}
+            >
+              <span>Sign In</span>
+              <span className='icon is-small'>
+                <i aria-hidden='true'>
+                  <FontAwesomeIcon icon={faAngleDown}></FontAwesomeIcon>
+                </i>
+              </span>
+            </button>
+          </div>
+          <div className='dropdown-menu' id='dropdown-menu6' role='menu'>
+            <div className='dropdown-content accent'>
+              <div className='dropdown-item'>
+                <form onSubmit={handleLogin}>
+                  <div className='field'>
+                    <label className='label font'>Email</label>
+                    <div className='control'>
+                      <input
+                        className='input primary font'
+                        placeholder='Your email'
+                        name='email'
+                        type='email'
+                        id='login_email'
+                        value={formState.email}
+                        onChange={handleChange}
+                      />
+                    </div>
                   </div>
-                </div>
 
-                <div className="field">
-                  <label className="label font">Password</label>
-                  <div className="control">
-                    <input
-                      className="input primary font"
-                      placeholder="******"
-                      name="password"
-                      type="password"
-                      id="login_password"
-                      value={formState.password}
-                      onChange={handleChange}
-                    />
+                  <div className='field'>
+                    <label className='label font'>Password</label>
+                    <div className='control'>
+                      <input
+                        className='input primary font'
+                        placeholder='******'
+                        name='password'
+                        type='password'
+                        id='login_password'
+                        value={formState.password}
+                        onChange={handleChange}
+                      />
+                    </div>
                   </div>
-                </div>
-                <button
-                  className="button primary signin-button-text font"
-                  type="submit"
-                >
-                  <Link
-                    to="/dashboard"
-                    onClick={() => {
-                      handleFormSubmit();
-                    }}
+                  <button
+                    className='button primary signin-button-text font'
+                    type='submit'
                   >
                     Sign In
-                  </Link>
-                </button>
-              </form>
-              {error && <div>Login failed</div>}
+                  </button>
+                </form>
+                {error && <div className='font is-size-7'>Login failed</div>}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }

a236c71d3d78021e5bb4a4f2afdd134ce2f79070
Edison Simondet <edison.simondet@gmail.com>
2022-03-22 17:35:45 -0500

    module upload dynamically

--- client/src/components/Project/index.js
+++ client/src/components/Project/index.js
@@ -55,12 +55,9 @@ function Project() {
             <h1 className='has-text-centered has-text-weight-semibold is-size-3 project-title'>
               Current Project
             </h1>
-            <div className='center section'>
-              {/* hardcoding index 0 once we are only handling 1 project at a time on our MVP */}
-              <h2 className='font'>{projects[0].projectTitle}</h2>
-              {/* temporary field to display data of issueCount for testing */}
-              <p>Issue count is: {projects[0].issueCount}</p>
-            </div>
+            <h2 className='center section is-size-3 project-title'>
+              {projects[0].projectTitle}
+            </h2>
             <div className='section center'>
               <h3 className='font'>Select a new project</h3>
             </div>
